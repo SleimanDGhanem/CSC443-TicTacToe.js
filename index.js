633f61778de27d9ce9b007f85a944d16be5c5d8a
Sleiman Ghanem <you@example.com>
2022-09-26 16:39:04 +0300

    created if else statement which implements minimax algorithm

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ let board = [
 ];
 
 let turnsLeft = 9;
-
 // value of player and opponent
 
-let player = "x",
+let isMax = true;
+
+const player = "x",
   opponent = "o";
 
 function evaluate() {
@@ -50,20 +51,35 @@ function evaluate() {
   return 0;
 }
 
-function miniMax() {
+function miniMax(isMax) {
   let score = evaluate();
 
   // added base condition
   if (score == -10 || score == 10) return score;
 
-  // if number of turns becomes 0 
-  if(turnsLeft == 0){
-    return 0
-  }
-  else{
-    turnsLeft--;
+  // if number of turns becomes 0
+  if (turnsLeft == 0) {
+    return 0;
   }
 
-  
+  if (isMax) {
+    let max = -1000;
 
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if ((board[i][j] = "_")) {
+          // make it AI value
+          board[i][j] = player;
+          // check if this is a good route
+          best = Math.max(best, miniMax(!isMax));
+          // undo route
+          board[i][j] = "_";
+        }
+      }
+    }
+    return best;
+  } else {
+    // if it is the player's turn
+    // let player pick a tic tac toe
+  }
 }
